decea2de7113bdda30882e2339e5d7e4602551c6
Daniel Galán <danielgalan0410@icloud.com>
2021-06-21 17:40:35 -0500

    testimonal section finished

--- js/index.js
+++ js/index.js
@@ -97,7 +97,7 @@ modalCloses.forEach((modalClose) => {
 
 //Portfolio Swiper
 
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
     navigation: {
@@ -108,4 +108,25 @@ let swiper = new Swiper(".portfolio__container", {
       el: ".swiper-pagination",
       clickable: true,
     },
+  });
+
+//Testimonial Swiper
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        },
+        768:{
+            slidesPerView: 3,
+        }
+    }
   });
